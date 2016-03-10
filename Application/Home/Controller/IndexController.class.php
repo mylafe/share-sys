eb55fae859a1f3b9f3f4e7bb338c5c1b1a012a24
Litao <1287862489@qq.com>
2016-03-10 20:44:11 +0800

    admin article

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -3,19 +3,28 @@ namespace Home\Controller;
 use Think\Controller;
 class IndexController extends CommonController {
     public function index(){
-    	// 获取当前用户的用户信息
+
+    	/*
+         *获取当前用户的用户信息
+         */
         $userid=$_SESSION['userid'];
         $info=M('userinfo')->where(array('userid' => $userid))->find();
         //var_dump($info);
         $this->assign('userinfo',$info);// 模板变量赋值
 
-        //获取文章信息
+        /*
+         *获取文章信息
+         */
         $arr=M('works')->order('time DESC')->select();
         //var_dump($arr);
         $this->assign('typeArr',$arr);// 模板变量赋值
-
-        //$data['title']= $this->msubstr();
-
+        //$data['title']= $this->msubstr();//函数赋值
+        
+        /*
+         *获取文章总数
+         */
+        $count=M('works')->count();
+        $this->assign('count',$count);//赋值
         $this->display();
     }
 
--- Application/Home/Controller/MyarticleController.class.php
+++ Application/Home/Controller/MyarticleController.class.php
@@ -8,6 +8,13 @@ class MyarticleController extends CommonController {
         // 我的文章
         $arr=M('works')->where(array('userid' => $userid))->order('time DESC')->select();
         $this->assign('typeArr',$arr);// 模板变量赋值
+
+        /*
+         *获取文章总数
+         */
+        $count=M('works')->where(array('userid' => $userid))->count();
+        $this->assign('count',$count);//赋值
+        
         $this->display();
     }
 
